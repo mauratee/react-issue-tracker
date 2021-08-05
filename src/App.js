a8b1eaf84671d87d86a9a5cca0599af29b08e6c8
mauratee <mauraethompson@gmail.com>
2021-08-05 19:42:36 +0000

    Add IssueItem components and update README.md

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import IssueItem from './components/IssueItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Issue Tracker
-      </header>
+    <div>
+        <IssueItem />
+        <IssueItem />
+        <IssueItem />
     </div>
   );
 }
---
+++ src/components/IssueItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './IssueItem.css'
+
+export default function IssueItem() {
+    return (
+        <div className="issue-item">
+            <h2>Issue Title</h2>
+            <div>Priority</div>
+            <div>Due Date</div>
+        </div>
+    )
+}
