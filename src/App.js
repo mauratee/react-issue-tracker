54f296e8650dcf91f4e4de2d77c1824b97fa1d50
mauratee <mauraethompson@gmail.com>
2021-08-05 21:36:09 +0000

    Add props from App to Issue Item and add formatting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
-import './App.css';
-import IssueItem from './components/IssueItem';
+import "./App.css";
+import IssueItem from "./components/IssueItem";
 
 function App() {
+  const issues = [
+    {
+      id: "i0",
+      description: "Make priority columns",
+      priority: "high",
+      date: new Date(2021, 7, 5),
+    },
+    {
+      id: "i1",
+      description: "Add new issue functionality",
+      priority: "high",
+      date: new Date(2021, 7, 6),
+    },
+    {
+      id: "i2",
+      description: "Delete issue functionality",
+      priority: "medium",
+      date: new Date(2021, 7, 5),
+    },
+    {
+      id: "i3",
+      description: "Toggle priority for each issue",
+      priority: "medium",
+      date: new Date(2021, 7, 4),
+    },
+  ];
+
   return (
     <div>
-        <IssueItem />
-        <IssueItem />
-        <IssueItem />
+      <header>Open Issue Tracker</header>
+      <div>
+        <IssueItem
+          id={issues[0].id}
+          description={issues[0].description}
+          priority={issues[0].priority}
+          date={issues[0].date}
+        />
+        <IssueItem 
+          id={issues[1].id}
+          description={issues[1].description}
+          priority={issues[1].priority}
+          date={issues[1].date}
+        />
+        <IssueItem 
+          id={issues[2].id}
+          description={issues[2].description}
+          priority={issues[2].priority}
+          date={issues[2].date}
+        />
+        <IssueItem 
+          id={issues[3].id}
+          description={issues[3].description}
+          priority={issues[3].priority}
+          date={issues[3].date}
+        />
+      </div>
     </div>
   );
 }
--- src/components/IssueItem.js
+++ src/components/IssueItem.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './IssueItem.css'
 
-export default function IssueItem() {
+export default function IssueItem(props) {
+    const date = props.date.toLocaleString('en-US', {dateStyle: 'short'})
+    
     return (
         <div className="issue-item">
-            <h2>Issue Title</h2>
-            <div>Priority</div>
-            <div>Due Date</div>
+            <h2>{props.description}</h2>
+            <div>{props.priority}</div>
+            <div>{date}</div>
         </div>
     )
 }
