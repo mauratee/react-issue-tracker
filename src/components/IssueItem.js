d648a056f3b5d02d6dfcb3db4057372f3534d6f2
mauratee <mauraethompson@gmail.com>
2021-08-10 17:35:51 +0000

    Add state to issue item

--- src/components/IssueItem.js
+++ src/components/IssueItem.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './IssueItem.css'
 
 
 export default function IssueItem(props) {
     const date = props.date.toLocaleString('en-US', {dateStyle: 'short'})
+
+    const [description, setDescription] = useState(props.description);
+
+    const clickHandler = () => {
+        setDescription('Updated!');
+        console.log(description)
+    }
     
     return (
         <div className="issue-item">
-            <h2>{props.description}</h2>
+            <h2>{description}</h2>
             <div>{props.priority}</div>
             <div>{date}</div>
+            <button onClick={clickHandler}>Change Title</button>
         </div>
     )
 }
